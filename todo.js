68e1e9d8c747a769a02c6414d0014f4557fb0e2a
MK S <conatuseus@gmail.com>
2020-06-01 19:30:16 +0900

    ToDo 리스트 뼈대 구현

---
+++ todo.js
@@ -0,0 +1,13 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+    toDoInput = toDoForm.querySelector("input"),
+    toDoList = document.querySelector(".js-toDoList");
+
+function loadToDos() {
+    
+}
+
+function init() {
+    loadToDos();
+}
+
+init();
