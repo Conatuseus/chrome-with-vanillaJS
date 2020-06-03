34de423782200a4b7de0c0d61348e2d2cbbfad09
MK S <conatuseus@gmail.com>
2020-06-03 14:47:23 +0900

    날씨 API 사용해서 현재 날씨정보 띄어주기

--- weather.js
+++ weather.js
@@ -1,6 +1,19 @@
-
+const weather = document.querySelector(".js-weather");
 const API_KEY = "15b4f215ee8a4b59fc4d3b15227d1e3b";
 const COORDS = "coords";
+
+
+function getWeather(lat, lng) {
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&&units=metric`
+    ).then(function(response){
+        return response.json();
+    }).then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    });
+}
 
 function saveCoords(coordsObj) {
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -14,6 +27,7 @@ function handleGeoSuccess(position) {
         longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -30,7 +44,8 @@ function loadCoords() {
         askForCoords();
     }
     else {
-        // getWeather
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
     }
 }
 
