dcd1baff57bdaf1ede9652990580f5e305a3ebbf
MK S <conatuseus@gmail.com>
2020-06-03 14:14:13 +0900

    위치 정보 저장 구현

---
+++ weather.js
@@ -0,0 +1,41 @@
+
+const API_KEY = "15b4f215ee8a4b59fc4d3b15227d1e3b";
+const COORDS = "coords";
+
+function saveCoords(coordsObj) {
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
+}
+
+function handleGeoError() {
+    console.log("error");
+}
+
+function askForCoords() {
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null) {
+        askForCoords();
+    }
+    else {
+        // getWeather
+    }
+}
+
+function init() {
+    loadCoords();
+}
+
+init();
